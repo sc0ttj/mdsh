0837c643ef6e07f89d326ac5351fa62b75d6a872
sc0ttj <scottj@uk2.net>
2019-02-20 15:33:49 +0800

    updated site

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 // custom app JS
 
-$(function () {
-  // DOM is ready now (all elements and styles loaded)
+// this site includes 'cash-dom' by default, so we can use it
+// for nicer DOM ready code:
+$(function() {
+  // DOM is ready now (all elements, styles and script loaded)
   // put your JS code here
-
 });
-
