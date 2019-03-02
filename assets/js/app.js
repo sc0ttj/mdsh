42975b87e900483edd8fc65457234a4f1ea70784
sc0ttj <scottj@uk2.net>
2019-03-02 18:34:37 +0800

    better site search JS: simpler, and also enables search on monthly/yearly index pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,38 +9,25 @@ $(function() {
   // site search, using Jets (https://jets.js.org/)
   if (Jets) {
     // check which page we are on - check which elems exist on the page
-    var archivePosts = document.getElementsByClassName("archive-posts")[0]; // archive pge
     var categories = document.getElementsByClassName("categories-list")[0]; // categories page
     var tags = document.getElementsByClassName("tags-list")[0]; // tags pge
-    var posts = document.getElementsByClassName("post-preview")[0]; // homepage
-    var postsMatchingCategory = document.getElementsByClassName(
-      "posts-in-category"
-    )[0]; // categories/<name>.html
-    var postsMatchingTag = document.getElementsByClassName(
-      "posts-matching-tag"
-    )[0]; // tags/<name>.html
-
+    var postPreviews = document.getElementsByClassName("post-preview")[0]; // homepage
+    var posts = document.getElementsByClassName("posts")[0]; // indexes
     var contentToSearch = "";
 
     // set the container elem to search
-    if (archivePosts) {
-      // archive page
-      contentToSearch = ".archive-posts";
-    } else if (categories) {
+    if (categories) {
       // categories page
       contentToSearch = ".categories-list";
     } else if (tags) {
       // tags page
       contentToSearch = ".tags-list";
-    } else if (postsMatchingCategory) {
-      // posts in category
-      contentToSearch = ".posts-in-category";
-    } else if (postsMatchingTag) {
-      // posts matching tag
-      contentToSearch = ".posts-matching-tag";
-    } else if (posts) {
+    } else if (postPreviews) {
       // homepage
       contentToSearch = "#content";
+    } else if (posts) {
+      // homepage
+      contentToSearch = ".posts";
     }
 
     //console.log("contentToSearch", contentToSearch);
