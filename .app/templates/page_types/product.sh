24d972e006be255352e90dd4dd5d74c7772fbf34
sc0ttj <sc0ttj@users.noreply.github.com>
2019-11-24 00:49:29 +0000

    small tweaks:
    - ask for currency before price when creating new products
    - remove taxonomies from page type "page", so it has none

--- .app/templates/page_types/product.sh
+++ .app/templates/page_types/product.sh
@@ -1,9 +1,9 @@
 # get the values we want in our front matter
-echo -n "Price: "
-read -er price_value
-
 echo -n "Currency (symbol): "
 read -er currency_value
+
+echo -n "Price: "
+read -er price_value
 
 # -a   sets indexed array
 # -g   makes it global (exports to env)
@@ -12,19 +12,19 @@ declare -ag front_matter_fields
 # declare the associative array names
 # -A   sets assoc array
 # -g   makes it global (exports to env)
-declare -Ag price_field
 declare -Ag currency_field
+declare -Ag price_field
 
-price_field=(
-  [varname]="product_price"
-  [value]="$price_value"
-)
 currency_field=(
   [varname]="product_currency"
   [value]="$currency_value"
 )
+price_field=(
+  [varname]="product_price"
+  [value]="$price_value"
+)
 
 # declare the array which will contain the *names* of our associative arrays
-front_matter_fields=(price_field currency_field)
+front_matter_fields=(currency_field price_field)
 
 export front_matter_fields
